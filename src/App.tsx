67df285c148a0eaaa248c00db6d99a83950121ac
Invra <identificationsucks@gmail.com>
2025-09-19 07:59:18 +1000

    Add: an initial test

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <main className="bg-zinc-900 flex flex-col justify-center text-zinc-100 h-screen overflow-hidden">
+      <h1 className="text-center text-3xl font-bold">Welcome to a ByteLab Tauri Rewrite</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      <div className="flex justify-center py-4  gap-4">
+        <a href="https://gitlab.com/bytelab-studio/ByteLab" target="_blank" className="select-none">
+          <img src="/bl.png" className="logo react w-25" draggable={false} alt="ByteLab Icon" />
         </a>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
