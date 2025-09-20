4f06ab6eaee88dc6719c39803824dcb6fda1fb7b
Invra <identificationsucks@gmail.com>
2025-09-20 20:46:38 +1000

    Fix some issues

--- src-tauri/package.nix
+++ src-tauri/package.nix
@@ -1,28 +1,42 @@
-{pkgs, naersk}:
-with pkgs;
-let
-  rust-toolchain = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
-in
-(callPackage naersk {
-  cargo = rust-toolchain;
-  rustc = rust-toolchain;
-}).buildPackage {
-  src = ./.;
-  cargoBuild = _: ''cargo $cargo_options tauri bundle $cargo_build_options >> $cargo_build_output_json -b deb'';
-  cargoBuildOptions = _: [];
-  nativeBuildInputs = [
-    gtk3    
-    librsvg
-    libsoup_3
+{
+  pkgs ? import <nixpkgs> {},
+  ...
+}:
+pkgs.stdenv.mkDerivation {
+  pname = "bytelab";
+  version = "0.1.0";
+
+  src = ../.;
+
+  nativeBuildInputs = with pkgs; [
+    bun
+    cargo
+    rustc
     pkg-config
-    gdk-pixbuf
-    cargo-tauri.hook
-  ] ++ lib.optionals stdenv.isLinux [
-    webkitgtk_4_1
-    glib-networking
-    openssl
-    webkitgtk_4_1
-    alsa-lib
-    wrapGAppsHook3
+    cargo-tauri
   ];
+
+  fetchPhase = ''
+    cd $src
+    bun install
+    cd src-tauri
+    cargo vendor
+  '';
+
+  buildPhase = ''
+    # cd src-tauri
+    cargo tauri build
+  '';
+
+  installPhase = if pkgs.stdenv.isDarwin then ''
+    #!/bin/bash
+    mkdir -p $out/bin
+    mkdir -p $out/Applications
+    cp -r target/release/bundle/macos/ByteLab.app $out/Applications
+    makeWrapper "$out/Applications/ByteLab.app/Contents/MacOS/ByteLab" \
+      "$out/bin/bytelab"
+  ''
+  else ''
+    #!/bin/bash
+  '';
 }
