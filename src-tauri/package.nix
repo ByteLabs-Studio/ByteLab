e099235b3286740a61a5522f2fefd84c9ac573c6
Invra <identificationsucks@gmail.com>
2025-09-21 01:08:43 +1000

    Fix derivation hopefully

--- src-tauri/package.nix
+++ src-tauri/package.nix
@@ -2,28 +2,38 @@
   pkgs ? import <nixpkgs> { },
   ...
 }:
-
-pkgs.stdenv.mkDerivation {
+with pkgs;
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "bytelab";
   version = "0.1.0";
 
   src = ../.;
+  cargoHash = "sha256-lv5HWhAOk00O4H6Mi93yLhB0b894lRgB8oy9OaRR/yU=";
+  cargoRoot = "src-tauri";
+  buildAndTestSubdir = finalAttrs.cargoRoot;
+
+  npmDeps = fetchNpmDeps {
+    name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
+    src = ../.;
+    hash = "sha256-BKQ6ejssLBoN64yUxBCv8rzo/F0CaDj71Av4u1ZP50Q=";
+  };
 
   nativeBuildInputs = with pkgs; [
     bun
     cargo
     rustc
+    nodejs
     pkg-config
     cargo-tauri
     makeWrapper
+    npmHooks.npmConfigHook
   ];
 
   buildPhase = ''
-    export HOME=$TMPDIR
-    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
-    export NIX_SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    # export HOME=$TMPDIR
+    # export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    # export NIX_SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
 
-    bun install --frozen-lockfile
     bun run build
     cargo tauri build
   '';
@@ -40,4 +50,4 @@ pkgs.stdenv.mkDerivation {
 
     cp -r src-tauri/target/release/bytelab $out/bin/bytelab
   '';
-}
+})
