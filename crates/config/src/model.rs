f3290a514e4474b0c2c68d63b14f0a5e9157e792
Invra <identificationsucks@gmail.com>
2026-01-23 19:30:11 +1100

    add: metronome and grid settings

--- crates/config/src/model.rs
+++ crates/config/src/model.rs
@@ -32,10 +32,18 @@ pub struct UserBehaviorOpts {
     pub init_track_auto_solo: Option<ArmBehaviors>,
     // On new track init, the track will be Record Armed.
     pub init_track_auto_arm: Option<ArmBehaviors>,
-    // Self-explanatory
+    // Self-explanatory.
     pub init_track_volume: Option<f32>,
-    // Self-explanatory
+    // Self-explanatory.
     pub init_track_pan: Option<f32>,
+    // Initial metronome speed.
+    pub init_project_bpm: Option<UserBpm>,
+    // Initial metronome time-signature.
+    pub init_project_time_signature: Option<UserTimeSignature>,
+    // Metronome count in time before track actually plays, 0 means instant.
+    pub init_project_bar_countin: Option<u8>,
+    // Arrangement view resolution: eg. "1/4", "1/8", "1/16"
+    pub init_arrangement_resolution: Option<String>,
 }
 
 pub struct BoolTrue(bool);
@@ -50,6 +58,21 @@ impl Deref for BoolTrue {
 impl Default for BoolTrue {
     fn default() -> Self {
         Self(true)
+    }
+}
+
+pub struct UserBpm(f32);
+
+impl Deref for UserBpm {
+    type Target = str;
+    fn deref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Default for UserBpm {
+    fn default() -> Self {
+        Self(120.0)
     }
 }
 
