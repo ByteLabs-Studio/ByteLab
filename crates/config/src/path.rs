3065dd270e17a9dd87d35a1dc6ece75b34e0f6a6
Invra <identificationsucks@gmail.com>
2026-01-25 17:44:09 +1100

    fix: some possible issues

--- crates/config/src/path.rs
+++ crates/config/src/path.rs
@@ -1,16 +1,22 @@
 use std::{path::PathBuf, str::FromStr};
 
 pub fn config_path() -> PathBuf {
-    if cfg!(unix) {
+    let path = if cfg!(unix) {
         std::env::var_os("XDG_CONFIG_HOME")
             .map(PathBuf::from)
             .unwrap_or_else(|| std::env::home_dir().unwrap_or_default().join(".config"))
-            .join("bytelabs")
-            .join("config.toml")
+            .join("bytelabs/config.toml")
     } else {
         PathBuf::from_str(&std::env::var("LOCALAPPDATA").unwrap())
             .unwrap()
-            .join("bytelabs")
-            .join("config.toml")
+            .join("bytelabs/config.toml")
+    };
+
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            std::fs::create_dir_all(parent).ok();
+        }
     }
+
+    path
 }
